321ac1910679aa9311ec8993960455b5ee9f696e
monomonedula <valh@tuta.io>
2020-06-18 10:58:52 +0300

    validation switch

--- test/test_delegate.py
+++ test/test_delegate.py
@@ -4,12 +4,13 @@ import pytest
 
 from abc_delegation.delegate import (
     DelegatingMeta,
-    delegation_metaclass,
+    delegation_metaclass, UnsafeDelegatingMeta,
 )
 from abc_delegation import multi_delegation_metaclass
 
 
-def test_basic_delegation():
+@pytest.mark.parametrize("metaclass", [DelegatingMeta, delegation_metaclass("_delegate")])
+def test_basic_delegation(metaclass):
     class A(metaclass=ABCMeta):
         @abstractmethod
         def bar(self):
@@ -26,7 +27,7 @@ def test_basic_delegation():
         def foo(self):
             return "B foo"
 
-    class C(A, metaclass=DelegatingMeta):
+    class C(A, metaclass=metaclass):
         def __init__(self, b):
             self._delegate = b
 
@@ -106,7 +107,8 @@ def test_custom_name_delegate():
     assert c.bar() == "B bar"
 
 
-def test_raises():
+@pytest.mark.parametrize("metaclass", [DelegatingMeta, delegation_metaclass("_delegate")])
+def test_raises(metaclass):
     class A(metaclass=ABCMeta):
         @abstractmethod
         def bar(self):
@@ -119,7 +121,7 @@ def test_raises():
     class B:
         pass
 
-    class C(A, metaclass=DelegatingMeta):
+    class C(A, metaclass=metaclass):
         def __init__(self, b):
             self._delegate = b
 
@@ -164,7 +166,8 @@ def test_raises_multi():
         C(B(), X())
 
 
-def test_partial_delegation():
+@pytest.mark.parametrize("metaclass", [DelegatingMeta, delegation_metaclass("_delegate")])
+def test_partial_delegation(metaclass):
     class A(metaclass=ABCMeta):
         @abstractmethod
         def bar(self):
@@ -178,7 +181,7 @@ def test_partial_delegation():
         def bar(self):
             return "B bar"
 
-    class C(A, metaclass=DelegatingMeta):
+    class C(A, metaclass=metaclass):
         def __init__(self, b):
             self._delegate = b
 
@@ -188,3 +191,60 @@ def test_partial_delegation():
     c = C(B())
     assert c.foo() == "C foo"
     assert c.bar() == "B bar"
+
+
+@pytest.mark.parametrize("metaclass", [UnsafeDelegatingMeta, delegation_metaclass("_delegate", validate=False)])
+def test_validation_turn_off(metaclass):
+    class A(metaclass=ABCMeta):
+        @abstractmethod
+        def bar(self):
+            pass
+
+        @abstractmethod
+        def foo(self):
+            pass
+
+    class B:
+        pass
+
+    class C(A, metaclass=metaclass):
+        def __init__(self, b):
+            self._delegate = b
+
+        def foo(self):
+            return "C foo"
+
+    C(B())
+
+
+def test_validation_turn_off_multi():
+    class A(metaclass=ABCMeta):
+        @abstractmethod
+        def bar(self):
+            pass
+
+        @abstractmethod
+        def foo(self):
+            pass
+
+        @abstractmethod
+        def baz(self):
+            pass
+
+    class B:
+        def foo(self):
+            return "B foo"
+
+    class X:
+        def baz(self):
+            return "X baz"
+
+    class C(A, metaclass=multi_delegation_metaclass("_delegate1", "_delegate2", validate=False)):
+        def __init__(self, d1, d2):
+            self._delegate1 = d1
+            self._delegate2 = d2
+
+        def foo(self):
+            return "C foo"
+
+    C(B(), X())
